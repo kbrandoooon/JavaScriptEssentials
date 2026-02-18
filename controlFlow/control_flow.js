e07f8d0b9ae7c4067de140642fb0588ca4770688
Brandon <perezbradd@gmail.com>
2026-02-17 22:38:42 -0500

    conditionals2

--- controlFlow/control_flow.js
+++ controlFlow/control_flow.js
@@ -45,3 +45,7 @@ switch (userType) {
         userCategory = "Unknown";
 }
 console.log("User Category:", userCategory);
+
+let isAuthenticated = true;
+let authenticationStatus = isAuthenticated ? "Authenticated" : "Not authenticated";
+console.log("Authentication Status:", authenticationStatus);
