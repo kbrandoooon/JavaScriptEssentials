72e667010de26ea397a48951dee4436bd00a7c29
Brandon Galindo <perezbradd@gmail.com>
2026-03-13 01:03:44 -0400

    speed analysis2

--- speedAnalysis/speed_analysis.js
+++ speedAnalysis/speed_analysis.js
@@ -27,6 +27,7 @@ function endTest() {
     // Calculate time elapsed and words per minute (WPM)
     var timeElapsed = (endTime - startTime) / 1000; // in seconds
     var userTypedText = document.getElementById("userInput").value;
+    var userTypedLength = userTypedText.length;
 
     // Split the text using regex to count words correctly
     var typedWords = userTypedText.split(/\s+/).filter(function (word) {
@@ -41,7 +42,9 @@ function endTest() {
 
     // Display the results
     var outputDiv = document.getElementById("output");
-    outputDiv.innerHTML = "<h2>Typing Test Results:</h2>" +
+    outputDiv.innerHTML =
+        "<h2>Typing Test Results:</h2>" +
+        "<p>Length Typed: " + userTypedLength + "</p>" +
         "<p>Words Typed: " + typedWords + "</p>" +
         "<p>Time Elapsed: " + timeElapsed.toFixed(2) + " seconds</p>" +
         "<p>Words Per Minute (WPM): " + wpm + "</p>";
